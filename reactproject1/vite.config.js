7c4aea642d031e7d07676f7874fcfe09151fd588
SmoothLeagueEnjoyer <xIzhax@gmail.com>
2025-01-31 12:33:57 -0500

    fix url

--- reactproject1/vite.config.js
+++ reactproject1/vite.config.js
@@ -3,7 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/urban-octo-garbanzo",
+    base: "/upgraded-palm-tree",
     plugins: [plugin()],
     server: {
         port: 52621,
